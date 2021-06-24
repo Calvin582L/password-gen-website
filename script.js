c00b7f8085a30b7a61dfd3a7f563d5b4231a25be
Calvin Liew <70549389+Calvin582L@users.noreply.github.com>
2021-06-24 16:56:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower_letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()?";
+const list_of_lower_letters = lower_letters.split("");
+const list_of_letters = letters.split("");
+const list_of_numbers = numbers.split("");
+const list_of_symbols = symbols.split("");
+
+let password_length = 7;
+let password = [];
+
+for (x = 0; x <= password_length; x++){
+let num = Math.floor(Math.random() * 4);
+if (num == 0){
+  password.push(list_of_letters[Math.floor(Math.random() * 26)])
+}
+else if (num == 1){
+  password.push(list_of_numbers[Math.floor(Math.random() * 10)])
+}
+else if (num == 2){
+  password.push(list_of_symbols[Math.floor(Math.random() * 11)])
+}
+
+else if (num == 3){
+  password.push(list_of_lower_letters[Math.floor(Math.random() * 26)])
+}
+}
+console.log(password.join(""));
+document.write(password.join(""));
